03b1c49a724eadf612a2c1c5dde604c3aa8ee902
Brie <Bss1089@gmail.com>
2025-11-02 20:40:50 -0500

    rotated sorted array

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 class Solution {
     /**
      * @param {number[]} nums
+     * @param {number} target
      * @return {number}
      */
-    findMin(nums) {
-        let l = 0;
-        let r = nums.length - 1;
-        let res = nums[0];
-
-        while (l <= r) {
-            if (nums[l] <= nums[r]) {
-                res = Math.min(res, nums[l]);
-                break;
-            }
-
-            let m = l + Math.floor((r - l) / 2);
-            res = Math.min(res, nums[m]);
-            if (nums[m] >= nums[l]) {
-                l = m + 1;
-            } else {
-                r = m - 1;
+    search(nums, target) {for (let i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
             }
         }
-        return res;
-    }
+        return -1;}
 }
